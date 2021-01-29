047443266a436ad495d6cee68d9d01bc97350bda
Tam Nguyen <ttamng0905@gmail.com>
2021-01-29 16:13:52 -0700

    add cluster

--- cluster.js
+++ cluster.js
@@ -8,6 +8,53 @@ if(cluster.isMaster){
     for(let i = 0; i < cpus; i++){
         cluster.fork();
     }
+
+    Object.values(cluster.workers).forEach(worker => {
+        worker.send(`Hello Worker ${worker.id}`)
+    });
+    const numberOfUsersInDB = function() {
+        this.count = this.count || 5;
+        this.count = this.count * this.count;
+        return this.count;
+    }
+
+    const updateWorkers = () => {
+        const usersCount = numberOfUsersInDB();
+        Object.values(cluster.workers).forEach(worker => {
+            worker.send({usersCount});
+        });
+    };
+
+    cluster.on('exit', (worker, code, signal) => {
+        if(code !== 0 && !worker.exitedAfterDisconnect){
+            console.log(`Worker ${worker.id} crashed.  ` +
+            `Starting a new worker...`);
+            cluster.fork();
+        }
+    });
+
+    // updateWorkers();
+    // setInterval(updateWorkers, 10000);
+    const workers = Object.values(cluster.workers);
+
+    const restartWorker = (workerIndex) => {
+        const worker = workers[workerIndex];
+        if(!worker) return;
+        worker.on('exit', ()=> {
+            if( !worker.exitedAfterDisconnect) return;
+            console.log(`Exited process ${worker.process.pid}`);
+
+            cluster.fork().on('listening', () => {
+                restartWorker(workerIndex + 1);
+            });
+        });
+        worker.disconnect();
+    };
+
+    restartWorker(0);
 } else {
     require('./server');
 }
+
+
+
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const http = require('http');
 const pid = process.pid;
 
+let usersCount;
+
 http.createServer((req, res) => {
     for(let i = 0; i < 1e7; i++){
-        res.end(`Handled by process ${pid}`);
+        res.write(`Handled by process ${pid}\n`);
+        res.end(`Users: ${usersCount}`);
     }
 }).listen(8080, () => {
     console.log(`Started process ${pid}`);
 })
+
+process.on('message', msg => {
+    usersCount = msg.usersCount;
+    console.log(`Message from master: ${usersCount}`);
+})
+
+// setTimeout(() => {
+//     process.exit(1) // death by random timeout
+// }, Math.random() * 10000);
+//
